d567ed12eede88b876c220533950d7b154af766f
SkAditAziz <38491593+SkAditAziz@users.noreply.github.com>
2021-05-05 13:10:18 +0600

    time limit completed

--- angulartest/ang.js
+++ angulartest/ang.js
@@ -1,27 +1,31 @@
 var app = angular.module("angApp",[]);
+var collectionDate1 = '2021-05-05T12:45:00'; 
+var collectionDate2 = '2021-05-05T12:59:00'; 
 
-app.controller("datecontroller", function($scope){
-    var collectionDate1 = '2021-05-04T09:00:00'; 
+app.controller('myCtrl', function($scope, $interval) {
     $scope.getDate1 = new Date(collectionDate1);
-
-    var collectionDate2 = '2021-05-04T10:00:00'; 
     $scope.getDate2 = new Date(collectionDate2);
-})
+    $scope.today = new Date();
+    
+    $scope.diff1 = ($scope.getDate1 - $scope.today)/1000;
+    console.log($scope.diff1);
+    $scope.diff2 = ($scope.getDate2 - $scope.today)/1000;
+    console.log($scope.diff2);
 
-app.controller('TimeCtrl', function($scope, $interval) {
-    var tick = function() {
-      $scope.clock = Date;
-    }
-    tick();
-    $interval(tick, 1000);
+    //$scope.diffinsec = getDate1
+    $scope.counter = Math.round($scope.diff2);   
+    $interval(function(){$scope.counter--},1000);
 });
 
-app.controller('myCtrl', function($scope, $interval) {  
-     $scope.counter = 3600;   
-     $interval(function(){console.log($scope.counter--)},1000);
-});
 app.filter('counter', [function() {
     return function(seconds) {
-        return new Date(0, 0, 0).setSeconds(seconds);
+        return new Date(0,0,0).setSeconds(seconds);
     };
 }])
+
+app.filter('inMilliseconds', function() {
+    return function(x) {
+              
+        return new Date(x).getTime();
+    };
+});
