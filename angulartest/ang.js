244af25116cf2f3d658e082095b3d8699570d8d2
SkAditAziz <38491593+SkAditAziz@users.noreply.github.com>
2021-05-06 16:18:46 +0600

    server with nodejs added

--- angulartest/ang.js
+++ angulartest/ang.js
@@ -1,6 +1,6 @@
 var app = angular.module("angApp",[]);
-var collectionDate1 = '2021-05-05T12:45:00'; 
-var collectionDate2 = '2021-05-05T12:59:00'; 
+var collectionDate1 = '2021-05-06T13:24:00'; 
+var collectionDate2 = '2021-05-07T13:24:55';
 
 app.controller('myCtrl', function($scope, $interval) {
     $scope.getDate1 = new Date(collectionDate1);
@@ -8,14 +8,39 @@ app.controller('myCtrl', function($scope, $interval) {
     $scope.today = new Date();
     
     $scope.diff1 = ($scope.getDate1 - $scope.today)/1000;
-    console.log($scope.diff1);
     $scope.diff2 = ($scope.getDate2 - $scope.today)/1000;
-    console.log($scope.diff2);
 
-    //$scope.diffinsec = getDate1
     $scope.counter = Math.round($scope.diff2);   
-    $interval(function(){$scope.counter--},1000);
+    $interval(function(){
+            $scope.counter--;
+    },1000);
+
+    $scope.filetype = ".pdf";
+
+    $scope.upload = function() {
+        var f = document.getElementById('file').files[0],
+            r = new FileReader();
+    
+        r.onloadend = function(e) {
+          var data = e.target.result;
+          //send your binary data via $http or $resource or do anything else with it
+        }
+    
+        r.readAsBinaryString(f);
+    }
 });
+
+app.directive("selectNgFiles", function() {
+    return {
+      require: "ngModel",
+      link: function postLink(scope,elem,attrs,ngModel) {
+        elem.on("change", function(e) {
+          var files = elem[0].files;
+          ngModel.$setViewValue(files);
+        })
+      }
+    }
+  });
 
 app.filter('counter', [function() {
     return function(seconds) {
